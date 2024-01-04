0f03d77588c097eacdd8e98e8611ff9179eb9c02
Euro Abo <abaoeuro2002@gmail.com>
2024-01-05 02:09:54 +0800

    leaderboard ui

--- client/src/components/LeaderBoard/LeaderBoard.jsx
+++ client/src/components/LeaderBoard/LeaderBoard.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import "./LeaderBoard.css";
 import { TiStarFullOutline } from "react-icons/ti";
 import { FaCrown } from "react-icons/fa6";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 function LeaderBoard() {
   return (
     <div className="leaderboard-container">
       <div className="leaderboard-wrapper">
-        <h1>Leaderboard</h1>
+        <div className="leaderboard-header">
+          <h1>Leaderboard</h1>
+        </div>
+        <div className="mode">
+          <label htmlFor="gameMode">Mode</label>
+          <select name="gameMode">
+            <option value="">Easy</option>
+            <option value="">Medium</option>
+            <option value="">Hard</option>
+            <option value="">Blind</option>
+            <option value="">Krazzy</option>
+            <option value="">Demon</option>
+          </select>
+        </div>
         <table>
           <thead>
             <tr>
@@ -66,8 +80,41 @@ function LeaderBoard() {
                 120 <TiStarFullOutline className="star" />
               </td>
             </tr>
+            <tr>
+              <td>7</td>
+              <td>greendLOG</td>
+              <td>
+                120 <TiStarFullOutline className="star" />
+              </td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>JR.</td>
+              <td>
+                120 <TiStarFullOutline className="star" />
+              </td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>platoMalamig</td>
+              <td>
+                120 <TiStarFullOutline className="star" />
+              </td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>grabeka098</td>
+              <td>
+                120 <TiStarFullOutline className="star" />
+              </td>
+            </tr>
           </tbody>
         </table>
+        <div className="pagination-buttons">
+          <IoIosArrowBack />
+          <p>3</p>
+          <IoIosArrowForward />
+        </div>
       </div>
     </div>
   );
