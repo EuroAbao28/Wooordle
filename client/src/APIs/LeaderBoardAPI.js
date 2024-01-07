20d1932b8b59f743cce07913d1ee42b6af462e8f
Euro Abo <abaoeuro2002@gmail.com>
2024-01-08 02:30:16 +0800

    changed to live server

--- client/src/APIs/LeaderBoardAPI.js
+++ client/src/APIs/LeaderBoardAPI.js
@@ -1,3 +1,4 @@
 export const host = "http://localhost:5000";
+export const live = "https://wooordle.onrender.com";
 
-export const leaderboardAPI = `${host}/api/leaderBoard/getLeaderBoard`;
+export const leaderboardAPI = `${live}/api/leaderBoard/getLeaderBoard`;
