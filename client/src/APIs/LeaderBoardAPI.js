0269a26f2f2efc5096f556f041ee95fac2a40fee
Euro Abo <abaoeuro2002@gmail.com>
2024-01-11 19:28:58 +0800

    change to live

--- client/src/APIs/LeaderBoardAPI.js
+++ client/src/APIs/LeaderBoardAPI.js
@@ -1,5 +1,5 @@
 export const host = "http://localhost:5000";
 export const live = "https://wooordle.onrender.com";
 
-export const leaderboardAPI = `${host}/api/leaderBoard/getLeaderBoard`;
-export const shareAPI = `${host}/api/leaderBoard/createLeaderBoard`;
+export const leaderboardAPI = `${live}/api/leaderBoard/getLeaderBoard`;
+export const shareAPI = `${live}/api/leaderBoard/createLeaderBoard`;
