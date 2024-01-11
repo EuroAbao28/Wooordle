f048e50ffba8f1b799293ff81baa489b267b5639
Euro Abo <abaoeuro2002@gmail.com>
2024-01-11 19:18:08 +0800

    api improve

--- server/controllers/leaderBoardController.js
+++ server/controllers/leaderBoardController.js
@@ -11,13 +11,26 @@ const getLeaderBoard = async (req, res) => {
 
     const leaderBoard = await leaderBoardModel
       .find({ mode })
+      .sort({ score: -1 }) // descending sort
       .skip((currentPage - 1) * listLimit)
       .limit(listLimit);
+
+    // Calculate rank for each entry
+    // .map ay gumagawa ka ng bagong array, nasa sayo na yan pano yung logic kada iterate
+    // sa logic na ito, kada iterate ay nilalagay mo yung orig copy ng kada object, which is yung entry by using spread operation.
+    // tapos dinadagdagan mo nalang ng logic for computing the rank
+    const rankedLeaderBoard = leaderBoard.map((entry, index) => ({
+      // 0 x 10 + 0 + 1 = rank 1
+      // bale current page at index lang nagdidictate dito kung ano rank
+      // isipin mo nalang ulit, alam kong malilimutan mo to e
+      ...entry.toObject(),
+      rank: (currentPage - 1) * listLimit + index + 1,
+    }));
 
     const totalEntry = await leaderBoardModel.countDocuments({ mode });
     const totalPages = Math.ceil(totalEntry / listLimit);
 
-    res.status(200).json({ leaderBoard, totalPages });
+    res.status(200).json({ leaderBoard: rankedLeaderBoard, totalPages });
   } catch (error) {
     console.error(error);
     res.json(error);
